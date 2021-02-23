eb85d49735e8aa86f693f4536b83dceedf3b532c
Sam Pullman <sampullman@gmail.com>
2021-02-23 12:30:21 +0800

    Update rollup config and packages, minify output

--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,19 @@
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import sourceMaps from 'rollup-plugin-sourcemaps';
+import { terser } from 'rollup-plugin-terser';
 
 const pkg = require('./package.json');
+
+const notMin = filename => filename.replace('.min', '');
 
 export default {
   input: 'out-tsc/index.js',
   output: [
-    { file: pkg.module, format: 'esm', sourcemap: true },
-    { file: pkg.main, format: 'cjs', sourcemap: true },
+    { file: pkg.module, format: 'esm', sourcemap: true, plugins: [terser()] },
+    { file: notMin(pkg.module), format: 'esm' },
+    { file: pkg.main, format: 'cjs', sourcemap: true, plugins: [terser()] },
+    { file: notMin(pkg.main), format: 'cjs' },
   ],
   external: [...Object.keys(pkg.devDependencies), 'debug'],
   plugins: [
